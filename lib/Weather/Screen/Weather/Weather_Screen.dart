2a92b676c99e49ebd028880d868adc6b36491507
pnkaj1999 <pankajgit525@gmail.com>
2024-07-31 15:39:07 +0530

    31/07/2024

---
+++ lib/Weather/Screen/Weather/Weather_Screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class WeatherScreen extends StatelessWidget {
+  const WeatherScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.fromLTRB(10, 15, 10, 0),
+          child: Column(
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.search),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
